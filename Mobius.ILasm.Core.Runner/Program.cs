63e0560c775005f06f6d92926222878381d615bb
Konrad Kokosa <kkokosa@f28-studio.com>
2021-07-28 22:21:31 +0200

    Simple command line parsing and second IL example.

--- Mobius.ILasm.Core.Runner/Program.cs
+++ Mobius.ILasm.Core.Runner/Program.cs
@@ -1,23 +1,53 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
+using PowerArgs;
 
 namespace Mobius.ILasm.Core.Runner
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             var logger = new Logger();
             var driver = new Driver(logger);
 
-            // TODO: make proper command args handling
-            // Maybe using https://github.com/adamabdelhamed/PowerArgs or sth else
-            using var memoryStream = new MemoryStream();
-            driver.Assemble(new string[] { "./resources/helloworldconsole.il", "/exe" }, memoryStream);
+            try
+            {
+                var parsedArgs = Args.Parse<Arguments>(args);
+                using var memoryStream = new MemoryStream();
+                driver.Assemble(new[] 
+                    { 
+                        parsedArgs.InputFile, 
+                        $"/{parsedArgs.OutputType}"
+                    }, 
+                    memoryStream);
 
-            using FileStream fileStream = new FileStream("file.exe", FileMode.Create, FileAccess.Write);
-            memoryStream.WriteTo(fileStream);
+                var outputFilename = parsedArgs.OutputFile ??
+                                     $"{Path.GetFileNameWithoutExtension(parsedArgs.InputFile)}.{parsedArgs.OutputType}";
+                using FileStream fileStream = new FileStream(outputFilename, FileMode.Create, FileAccess.Write);
+                memoryStream.WriteTo(fileStream);
+            }
+            catch (ArgException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ArgUsage.GenerateUsageFromTemplate<Arguments>());
+            }
         }
+    }
+
+    public class Arguments
+    {
+        // This argument is required and if not specified the user will 
+        // be prompted.
+        [ArgRequired, ArgDescription("IL file to be parsed"), ArgExistingFile]
+        public string InputFile { get; set; }
+
+        public string OutputFile { get; set; }
+
+        // This argument is not required, but if specified must be >= 0 and <= 60
+
+        [ArgRegex("exe|dll"), ArgDefaultValue("exe")]
+        public string OutputType { get; set; }
     }
 }
